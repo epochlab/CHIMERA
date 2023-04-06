8f7bf627405873cfd92ea51d1528ad530acce256
James Roberts <JRoberts86@gmail.com>
2023-04-06 22:32:20 +0100

    Add new fasta and update profiles

--- profiles/SARS_CoV_2.py
+++ profiles/SARS_CoV_2.py
@@ -5,13 +5,41 @@
 # Dataset: NCBI RefSeq SARS-CoV-2 genome sequence record: https://www.ncbi.nlm.nih.gov/nuccore/NC_045512
 
 # Mojiang (Bengping) Copper Mine: N23°10'36' E101°21'28'
-# https://s3.documentcloud.org/documents/6981198/Analysis-of-Six-Patients-With-Unknown-Viruses.pdf - Lu Xi Masters Thesis (2013)
+# https://s3.documentcloud.org/documents/6981198/Analysis-of-Six-Patients-With-Unknown-Viruses.pdf - Lu Xi, Masters Thesis (2013)
 # 1,885km Bengpinghe > WIV
-
-
 
 # Immunoglobulin-M (IgM) Antibodies = Recent exposure
 # IgG Antibodies = Created during initial infection but last for months
+
+# June Almeida (1963)
+
+# Coronavirus' are divided into 4 groups based on genetic difference
+# Alpha, Beta, Gamma & Delta
+
+# Alpha and Beta = Common
+# Gamma and Delta = Rare
+
+# Within the beta subdivision there are 3 different species:
+# Lineage B
+# SARS-like or SARS-related (SARSr)
+# Sarbecoviruses
+
+# Both SARS-1 and SARS-CoV-2 are both Sarbeviruses 
+# Coronaviruses known to infect humans: 229E, NL63, HKU1, OC43, MERS, SARS-1, SARS-Cov-2
+
+# Synonymous vs Non-synonymous
+# Does NOT alter the residue chain = Synonymous
+# DOES alter the residue chain = Non-Synonymous
+# Caused by redundancy in the nucleotide sequence as some codons make the same protein.
+
+# SARS-CoV-2 affects both upper and lower respiratory system.
+# When in lungs and lower respiratory system.
+# Affecting alveoli resulting in opaque shadows on scans.
+
+# Severe reactions can trigger 'cytokine storm'.
+# Inflammation caused by overracting immune system leading to irresversible tissue damage.
+# Affects other organs inc. intestines, heart, eyes, blood, sperm and CNS.
+# The infection is systemic and damage kidney, liver and spleen.
 
 # -----
 
@@ -52,6 +80,10 @@ ORF8 = translate(genome[27894-1: 28259], codon_table)                           
 N = translate(genome[28274-1: 29533], codon_table)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
 ORF10 = translate(genome[29558-1: 29674], codon_table)                                       # ORF10 protein - 38
 # print(S)
+
+# Angiotensin Converting Enzyme-II (ACE2)
+# Receptor-Binding-Domain (RBD)
+# ACE2 modulates blood pressure
 
 # Identify FURIN cleavage site in spike protein
 print('FURIN cleavage site (Spike):', S.find('PRRAR'))

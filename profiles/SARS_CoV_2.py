e732b1213c59b03a98cc69e879c50aa598d3b6d5
James Roberts <JRoberts86@gmail.com>
2023-10-07 20:30:38 +0100

    Split libtools into classes

--- profiles/SARS_CoV_2.py
+++ profiles/SARS_CoV_2.py
@@ -37,10 +37,12 @@ import sys
 sys.path.append('..')
 
 import zlib
-from libtools import *
+from libtools import FastaIO
 from codon import RNA
 
-label, genome = load('../genome/NC_045512.2.fasta')
+FASTA = FastaIO()
+
+label, genome = FASTA.load('../genome/NC_045512.2.fasta')
 # print(label, genome)
 
 codon_table = RNA()
@@ -53,7 +55,7 @@ print('Compression (zlib):', len(zlib.compress(genome.encode("utf-8"))))
 # Sequential CGG position - Does NOT align with a modulo of 3, check reading_frame - ???
 print('CpG Islands:', genome.find('CGGCGG'))
 
-res = translate(genome, codon_table).split('*')
+res = FASTA.translate(genome, codon_table).split('*')
 # print(res)
 
 # 15 genes encode 29 proteins
@@ -63,18 +65,18 @@ res = translate(genome, codon_table).split('*')
 # nsp14 proof reads the duplicate virus for error-checking. 
 
 # Reverse-engineered proteins
-ORF1a = translate(genome[266-1: 13483], codon_table)                                         # ORF1a polyprotein - 4405
-ORF1b = translate(genome[13468-1: 21555], codon_table)                                       # ORF1b polyprotein - 2695 overlapping sequence w/ ORF1a
-S = translate(genome[21563-1: 25384], codon_table)                                           # Spike glycoprotein (structural) - 1273
-ORF3a = translate(genome[25393-1: 26220], codon_table)                                       # ORF3a protein - 275
-E = translate(genome[26245-1: 26472], codon_table)                                           # ORF4 envelope protein (structural) - 75
-M = translate(genome[26523-1: 27191], codon_table)                                           # ORF5 membrane glycoprotein (structural) - 222
-ORF6 = translate(genome[27202-1: 27387], codon_table)                                        # ORF6 protein - 61
-ORF7a = translate(genome[27394-1: 27759], codon_table)                                       # ORF7a protein - 121
-ORF7b = translate(genome[27756-1: 27887], codon_table)                                       # ORF7b protein - 43
-ORF8 = translate(genome[27894-1: 28259], codon_table)                                        # ORF8 protein - 121
-N = translate(genome[28274-1: 29533], codon_table)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
-ORF10 = translate(genome[29558-1: 29674], codon_table)                                       # ORF10 protein - 38
+ORF1a = FASTA.translate(genome[266-1: 13483], codon_table)                                         # ORF1a polyprotein - 4405
+ORF1b = FASTA.translate(genome[13468-1: 21555], codon_table)                                       # ORF1b polyprotein - 2695 overlapping sequence w/ ORF1a
+S = FASTA.translate(genome[21563-1: 25384], codon_table)                                           # Spike glycoprotein (structural) - 1273
+ORF3a = FASTA.translate(genome[25393-1: 26220], codon_table)                                       # ORF3a protein - 275
+E = FASTA.translate(genome[26245-1: 26472], codon_table)                                           # ORF4 envelope protein (structural) - 75
+M = FASTA.translate(genome[26523-1: 27191], codon_table)                                           # ORF5 membrane glycoprotein (structural) - 222
+ORF6 = FASTA.translate(genome[27202-1: 27387], codon_table)                                        # ORF6 protein - 61
+ORF7a = FASTA.translate(genome[27394-1: 27759], codon_table)                                       # ORF7a protein - 121
+ORF7b = FASTA.translate(genome[27756-1: 27887], codon_table)                                       # ORF7b protein - 43
+ORF8 = FASTA.translate(genome[27894-1: 28259], codon_table)                                        # ORF8 protein - 121
+N = FASTA.translate(genome[28274-1: 29533], codon_table)                                           # ORF9 nucleocapsid phosphoprotein (structural) - 419
+ORF10 = FASTA.translate(genome[29558-1: 29674], codon_table)                                       # ORF10 protein - 38
 print(S)
 
 # Angiotensin Converting Enzyme-II (ACE2)
